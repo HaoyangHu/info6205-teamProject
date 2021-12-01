da664a34827c58703bbe60ecd631aaaa13d5f2a8
carlruan <carlruan981024@gmail.com>
2021-12-01 09:52:52 -0500

    implement timsort

--- src/test/java/sortWithCollator/TimSortTest.java
+++ src/test/java/sortWithCollator/TimSortTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class TimSortTest {
     @Test
     public void test(){
-        /*String[] test = new String[]{"刘备", "张三", "李四", "王五", "赵六"};
-        TimSort.sort(test);
+        String[] test = new String[]{"刘备", "张三", "李四", "王五", "赵六"};
+        TimSort.sort(test, test.length);
 
         assert(test[0].equals("李四"));
         assert(test[1].equals("刘备"));
         assert(test[2].equals("王五"));
         assert(test[3].equals("张三"));
-        assert(test[4].equals("赵六"));*/
+        assert(test[4].equals("赵六"));
     }
 
 }
