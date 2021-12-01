aea4a6c16c52e410a1bf44c9e33dd8acf4b2d857
hu.haoy@northeastern.edu <haoyanghu@HAOYANGdeMBP.cable.rcn.com>
2021-12-01 18:12:56 -0500

    implement benchmark

--- src/main/java/benchmark/PinYin4jBenchmark/TimeSortBenchmark.java
+++ src/main/java/benchmark/PinYin4jBenchmark/TimeSortBenchmark.java
@@ -1,2 +1,23 @@
-package benchmark.PinYin4jBenchmark;public class TimeSortBenchmark {
+package benchmark.PinYin4jBenchmark;
+
+import benchmark.Utils.Benchmark_Timer;
+import benchmark.Utils.TwoDimensionalArrayLengthChange;
+import sortWithPinYin4j.TimeSort;
+
+import java.util.Arrays;
+
+public class TimeSortBenchmark {
+    public static void main(String[] args) {
+
+//        String[][] aQuarterOfNames = TwoDimensionalArrayLengthChange.generateAQuarterOfFiles();
+//        TimeSort.sort(aQuarterOfNames);
+//        System.out.println(Arrays.deepToString(aQuarterOfNames));
+
+        for (int i = 0; i < 8; i++) {
+            String[][] aQuarterOfNames = TwoDimensionalArrayLengthChange.generateAHalfOfFiles();
+            Benchmark_Timer benchmark_timer = new Benchmark_Timer(" 250k test: ", null , (t)-> sortWithPinYin4j.TimeSort.sort(aQuarterOfNames), null );
+            System.out.println(benchmark_timer.run(true, 100));
+        }
+
+    }
 }
--- src/main/java/benchmark/Utils/ArrayLengthChange.java
+++ src/main/java/benchmark/Utils/ArrayLengthChange.java
@@ -1,2 +1,16 @@
-package benchmark.Utils;public class ArrayLengthChange {
+package benchmark.Utils;
+
+import preprocess.NameData;
+
+public class ArrayLengthChange {
+    public static String[] generateAQuarterOfFiles(){
+        String[] aQuarterOfNames;
+        int len = NameData.len / 4;
+        aQuarterOfNames = new String[len];
+        for(int i = 0; i < len; i++){
+            aQuarterOfNames[i] = NameData.names.get(i).name;
+        }
+        return aQuarterOfNames;
+    }
+
 }
